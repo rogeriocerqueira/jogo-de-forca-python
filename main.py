05d6f112118f0c7b7b5094554fa5e440ff1d4dc2
Rogério Cerqueira <rogeriocerqueira.uefs@gmail.com>
2021-05-14 14:17:50 -0300

    Versão Final
    Coloquei nesse último branch a versão final do código
    funcionando com todas as alterações proposta.

--- main.py
+++ main.py
@@ -52,23 +52,18 @@ HANGMANPICS = ['''
 =========''']
 
 def escolhe_palavra():
-    with open('palavras') as arquivo:
-        
-        linha_aleatoria = random.randrange(1,sum(1 for _ in arquivo)) # Seleciona uma linha aleartória do arquivo
-
-        #Busca palavra específica
-        arquivo = open('palavras', 'r')
-        palavra = (arquivo.readlines()[linha_aleatoria])
-        arquivo.close
-        return palavra
+        with open('palavras') as arquivo:
+            linhas = arquivo.readlines()
+            sorteia = random.randrange(1, len(linhas))
+            linhas = linhas[sorteia]
+            return linhas[:-1]
 
 def esconde_palavra():
     palavra = escolhe_palavra()
     tracos = []
-    for caracter in palavra:
+    for _ in palavra:
         tracos.append('_')
     return palavra, tracos
-
 
 def verifica_jogo(acertos, erros, palavra):
     if acertos >= len(palavra) and erros != len(HANGMANPICS)-1:
@@ -79,40 +74,43 @@ def verifica_jogo(acertos, erros, palavra):
         print('A palavra era: %s ' %(palavra))
 
 
-def inicia_jogo():
-    palavra, tracos = esconde_palavra()
-    acertos, erros = 0, 0
+def analisa_letra(escolha, lista): # Guarda as letras escolhidas pelo usuario em uma lista de valores
 
-    while (erros != len(HANGMANPICS)):
+    for caracter in lista:
+        if escolha == caracter:
+            return True, lista
+
+    lista.append(escolha)
+    return False, lista
+
+
+def inicia_jogo():
+    print(HANGMANPICS[0]) #Mostra a forca logo de inicio
+    palavra, tracos = esconde_palavra()
+    print(''.join(tracos))
+    acertos, erros = 0, 0
+    lista = ['*']  # Aqui eu inicializei a lista com um valor qualquer só pra não ocorrer erro
+
+    while (erros != len(HANGMANPICS)-1) and acertos < len(palavra):
         i = 0
         escolha = input('Digite uma letra: ').upper()
 
         if escolha in palavra:
-            for caracter in palavra:
+            for _ in palavra:
                 if escolha in palavra[i]:
-                    tracos[i] = caracter
+                    tracos[i] = _
                     acertos += 1
                 i+=1
 
         else:
-            print(HANGMANPICS[erros+1]) #Imprime a posição seguinte da lista e não a primeira
+            print(HANGMANPICS[erros+1]) #Proximo elemento da lista
             erros +=1
 
-        if acertos >= len(palavra) - 1: #Excluindo o caracter "\n" presente no final da string do arquivo
-            break
+        print(''.join(tracos))
+        print(acertos, (len(palavra)))
 
-        #print(palavra) #só pra conferir se to fazendo certo
-        # print(acertos, len(palavra)-1)
-
-        print(''.join(tracos[:-1]))
     verifica_jogo(acertos, erros, palavra)
-
-def main():
-    inicia_jogo()
 
 if __name__ == '__main__':
     inicia_jogo()
-
-#Coisas que faltam segundo @Cussa me indicou na mentoria
-# 1 - Verificar se o usário não digitou a letra e esta digitando novamente a mesma letra
-# 2 - Verificar se o usuário digitou um caracter inválido ou algo desse tipo.
+    # escolhe_palavra()
